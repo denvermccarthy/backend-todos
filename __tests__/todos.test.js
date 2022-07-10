dd8861548055e0d2f33df62f21c93366373affaf
Denver McCarthy <dmichaelspdx@gmail.com>
2022-07-09 20:21:08 -0700

    added function to test that should create data to test with

--- __tests__/todos.test.js
+++ __tests__/todos.test.js
@@ -2,10 +2,16 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Todo = require('../lib/models/Todo');
 const mockUser = {
   email: 'test@testing.net',
   password: 'password',
 };
+const todos = [
+  { todo: 'Take the trash out' },
+  { todo: 'Clean my room' },
+  { todo: 'Finish this assignment' },
+];
 
 const registerAndLogin = async () => {
   const agent = request.agent(app);
@@ -14,13 +20,18 @@ const registerAndLogin = async () => {
   await agent.post('/api/v1/users/session').send(mockUser);
   return [agent, user.body];
 };
+const createDummyData = async () => {
+  const userData = await registerAndLogin();
+  await Promise.all(todos.map((todo) => Todo.insertTodo(todo)));
+  return userData;
+};
 
 describe('user routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  test.skip('GET to /todos should list all todos for the authed user.', async () => {
-    const [agent] = await registerAndLogin();
+  test('GET to /todos should list all todos for the authed user.', async () => {
+    const [agent] = await createDummyData();
     const res = await agent.get('/api/v1/todos');
     expect(res.body.length > 0).toEqual(true);
     expect(res.body[0]).toEqual({
