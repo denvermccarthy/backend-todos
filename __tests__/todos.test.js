b16eae723012dc7d4afa4d9357a34ad59409fef0
Denver McCarthy <dmichaelspdx@gmail.com>
2022-07-09 19:40:15 -0700

    added test for get route

---
+++ __tests__/todos.test.js
@@ -0,0 +1,38 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const mockUser = {
+  email: 'test@testing.net',
+  password: 'password',
+};
+
+const registerAndLogin = async () => {
+  const agent = request.agent(app);
+  const user = agent.post('/api/v1/users').send(mockUser);
+
+  await agent
+    .post('/api/v1/users/sessions')
+    .send({ email: mockUser.password, password: mockUser.password });
+  return [agent, user];
+};
+
+describe('user routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  test('GET to /todos should list all todos for the authed user.', async () => {
+    const [agent] = await registerAndLogin();
+    const res = await agent.get('/api/v1/todos');
+    expect(res.body.length > 0).toEqual(true);
+    expect(res.body[0]).toEqual({
+      id: expect.any(String),
+      todo: expect.any(String),
+      done: expect.any(Boolean),
+    });
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
